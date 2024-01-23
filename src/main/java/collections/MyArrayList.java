25e25a61bfac7bc2df0c49639433c40bb1c9d91f
michelle <michelle.graham@dkit.ie>
2024-01-23 11:01:04 +0000

    Added basic ArrayList class and : size() grow() add()

---
+++ src/main/java/collections/MyArrayList.java
@@ -0,0 +1,37 @@
+package collections;
+
+public class MyArrayList {
+
+    private int [] data;
+    private int num_elements;
+
+    public MyArrayList(){
+        data = new int[10];
+    }
+
+    public int size(){
+        return num_elements;
+    }
+
+    public void add(int num){
+        if(num_elements == data.length){
+            grow();
+        }
+        
+        data[num_elements] = num;
+        num_elements++;
+    }
+
+    // Grow
+    // create new array with X extra slots
+    // loop through original array and copy to new array
+    // Replace the original array with the new one
+    private void grow(){
+        int [] tempArray = new int[data.length + 10];
+        for(int i = 0; i < data.length; i++){
+            tempArray[i] = data[i];
+        }
+
+        data = tempArray;
+    }
+}
